30d0e5e2782e46ba0958901efe2dc7469d948696
SimpleCodeCX <248200851@qq.com>
2019-07-12 02:20:32 +0800

    feat(dialog-demo): rich dialog's style

--- demo1/dialog.js
+++ demo1/dialog.js
@@ -4,9 +4,10 @@
   const dialogTemplate = `
   <div id="dialog-container" class="dialog-container">
     <div id="dialog" class="dialog" draggable="true" ondragstart="dragDialogStart(event)" ondragend="dragDialogEnd(event)">
+      <div class="dialog-bar"></div>
       <div id="dialog-title" class="dialog-title"></div>
       <div id="dialog-text" class="dialog-text"></div>
-      <div>
+      <div class="dialog-btn-box">
         <button id="dialog-cancel-btn" class="dialog-btn">取消</button>
         <button id="dialog-confirm-btn" class="dialog-btn">确定</button>
       </div>
